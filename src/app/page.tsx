04a4b8865c14a970dc1094e38dca78a230a2595a
Ben Mallett <ben@Bens-MacBook-Air.local>
2024-06-05 16:48:59 -0400

    updating keys + fixing build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,19 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import axios from "axios";
 import { useToast } from "@/components/ui/use-toast";
+import { getRandomBgColor } from "@/lib/utils";
 
 export default function Home() {
   const { toast } = useToast()
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<any[]>([]);
+  const [showcaseItems, setShowcaseItems] = useState<any[]>([]);
 
   useEffect(() => {
     const getPosts = async () => {
       try {
         const response = await axios.get("/api/posts");
         const postsResponse = response.data.blogposts;
-        setPosts(postsResponse)
+        setPosts(postsResponse.slice(0, 3))
       } catch (error : any) {
         toast({
             title: "Error fetching blog posts",
@@ -29,10 +31,29 @@ export default function Home() {
     });
   }, [toast])
 
+  useEffect(() => {
+    const getShowcaseItems = async () => {
+        try {
+            const response = await axios.get("/api/showcase");
+            const showcaseResponse = response.data.showcaseItems;
+            setShowcaseItems(showcaseResponse.slice(0,3))
+        } catch (error : any) {
+            toast({
+                title: "Error fetching showcase items",
+                description: `${error.response.data.message ?? error}`
+            })
+        }
+    }
+
+    getShowcaseItems().catch((e) => {
+        console.log(e)
+    });
+  }, [toast])
+
   return (
     <main>
       <Header/>
-      <div className="w-full h-full pt-20 flex flex-col justify-center items-center gap-6 px-10">
+      <div className="w-full h-full pt-10 flex flex-col justify-center items-center gap-6 px-10">
         <h1 className="text-6xl">Hey! My name is Ben</h1>
         <div className="w-4/5/3 text-4xl text-center">
           I read things. I write things. I build things. You can read about some of those things here.
@@ -50,46 +71,54 @@ export default function Home() {
             <span className="text-2xl font-semibold w-1/4 border border-black border-2 px-2 py-1 bg-green-300">contact</span>
           </Link>
         </div>
-        <div className="flex flex-col justify-start items-center pt-20">
-          <h2>Recent Blogposts</h2>
-          <div className="flex justify-center items-center gap-2">
-            {
-              posts.map((post, i) => {
-                return (
-                  <div className="w-[200px] h-[200px] bg-red-400 border border-black border-2 hover-shadow-sm">
-                    Something
-                  </div>
-                )
-              })
-            }
-            <Link href='/blog'>
-              <div className="w-[200px] h-[200px] bg-white border border-black border-2 flex justify-center items-center hover-shadow-sm text-2xl p-2">
-                &rarr;
-                <br/>
-                To the blog
-              </div>
-            </Link>
+        <div className="flex flex-col lg:flex-row justify-start items-start gap-10">
+          <div className="flex flex-col justify-start items-center pt-10">
+            <h2>Recent Blogposts</h2>
+            <div className="grid grid-cols-2 justify-center items-center gap-6 p-6">
+              {
+                posts.map((post, i) => {
+                  const bgColor = getRandomBgColor()
+                  return (
+                    <Link key={i} href={`/blog/${post.slug}`}>
+                      <div className={`w-[200px] h-[200px] border border-black border-2 hover-shadow-sm flex justify-center items-center ${bgColor} text-2xl`}>
+                        {post.title}
+                      </div>
+                    </Link>
+                  )
+                })
+              }
+              <Link key={i} href='/blog'>
+                <div className="w-[200px] h-[200px] bg-white border border-black border-2 flex justify-center items-center hover-shadow-sm text-2xl p-2">
+                  &rarr;
+                  <br/>
+                  To the blog
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col justify-start items-center pt-20">
-          <h2>Recent Projects</h2>
-          <div className="flex justify-center items-center gap-2">
-            {
-              posts.map((post, i) => {
-                return (
-                  <div className="w-[200px] h-[200px] bg-red-400 border border-black border-2 hover-shadow-sm">
-                    Something
-                  </div>
-                )
-              })
-            }
-            <Link href='/blog'>
-              <div className="w-[200px] h-[200px] bg-white border border-black border-2 flex justify-center items-center hover-shadow-sm text-2xl p-2">
-                &rarr;
-                <br/>
-                To the Showroom
-              </div>
-            </Link>
+          <div className="flex flex-col justify-start items-center pt-10">
+            <h2>Recent Projects</h2>
+            <div className="grid grid-cols-2 justify-center items-center gap-6 p-6">
+              {
+                showcaseItems.map((showcase, i) => {
+                  const bgColor = getRandomBgColor()
+                  return (
+                    <Link href={`/showcase/${showcase.slug}`}>
+                      <div className={`w-[200px] h-[200px] border border-black border-2 hover-shadow-sm flex justify-center items-center ${bgColor} text-2xl`}>
+                        {showcase.title}
+                      </div>
+                    </Link>
+                  )
+                })
+              }
+              <Link href='/blog'>
+                <div className="w-[200px] h-[200px] bg-white border border-black border-2 flex justify-center items-center hover-shadow-sm text-2xl p-2">
+                  &rarr;
+                  <br/>
+                  To the Showroom
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
