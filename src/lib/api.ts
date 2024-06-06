74fc7b5842277de005ae68a35e3d4eb8905a365a
Ben Mallett <ben@Bens-MacBook-Air.local>
2024-06-05 20:20:49 -0400

    updating to include only valid projects

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -80,16 +80,6 @@ export const showcaseItems = [
         description: "A unique plaground showcasing John Conway's famous 'Game of Life', and some of the emergent behavior that happens when you change the rules.",
         slug: "conway"
     },
-    {
-        title: "Falling Sands",
-        description: "A cellular automata with varieties of materials and unique interactions. Build, destroy, and experience the majesty of simple cellular rules.",
-        slug: "sands"
-    },
-    {
-        title: "Tetris?",
-        description: "The classic game of Tetris with a more tetrominos and some fun new twists! Take on variable board size, speed, point mechanics, and more!",
-        slug: "tetris"
-    },
 ]
 
 export async function getAllShowcaseItems() {
